1a3a97381b37acffd67994d91b20053a3b368c39
Himalaya <yugmani@yahoo.com>
2022-01-18 13:53:36 -0800

    javascript

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
+// Variable initializations
 let currentQuestion = 0;
-const questionEl = document.getElementById("question");
 let score = 0;
 
 const text_a = document.getElementById("text-a");
@@ -7,52 +7,105 @@ const text_b = document.getElementById("text-b");
 const text_c = document.getElementById("text-c");
 const text_d = document.getElementById("text-d");
 
+const questionEl = document.getElementById("question");
 const submitBtn = document.getElementById("submit");
-
 const radioEl = document.querySelectorAll('input[type="radio');
-console.log(radioEl);
 
+//function to load quiz
 function loadQuiz() {
   const currentQuizData = quizData[currentQuestion];
+
   questionEl.innerHTML = currentQuizData.question;
   text_a.innerHTML = currentQuizData.a;
   text_b.innerHTML = currentQuizData.b;
   text_c.innerHTML = currentQuizData.c;
   text_d.innerHTML = currentQuizData.d;
-  checkSelected();
+
+  return currentQuizData;
 }
 
-loadQuiz();
+// function to check whether answer is selected or not
+function isAnswerSelected() {
+  const checkedId = checkSelected();
+  let qScore = 0;
 
-submitBtn.addEventListener("click", function () {
-  currentQuestion++;
-  if (currentQuestion < quizData.length) {
-    loadQuiz();
+  if (checkedId) {
+    qScore = isCorrectResponse(checkedId, currentQuiz);
+    currentQuestion++;
   } else {
-    alert("Thank you");
+    return;
   }
-});
 
+  // to load next question
+  if (currentQuestion < quizData.length) {
+    //to set uncheck for checked radios by default
+    radioEl.forEach((item) => {
+      if (item.checked) {
+        item.checked = false;
+      }
+    });
+
+    currentQuiz = loadQuiz();
+  } else {
+    showResult(qScore);
+  }
+}
+
+// function to find which answer is checked.
 function checkSelected() {
   let checkedId = "";
+
   radioEl.forEach((item) => {
     if (item.checked) {
       checkedId = item.getAttribute("id");
     }
   });
 
-  if (!checkedId) {
-    return;
-  }
-
-  isCorrectResponse(checkedId);
+  return checkedId;
 }
 
-function isCorrectResponse(id) {
-  if (id === currentQuizData[currentQuestion].correct) {
+// function to check whether the selected answer is correct or not
+function isCorrectResponse(id, current) {
+  if (id === current.correct) {
     score += 5;
-    alert("You are right!");
   }
-  console.log(score);
+
   return score;
 }
+
+// function to show the result at the end of quiz
+function showResult() {
+  document.querySelector(".container").style.display = "none";
+
+  const resultEl = document.getElementById("result");
+  resultEl.style.display = "block";
+  resultEl.style.display = "flex";
+  resultEl.style.flexDirection = "column";
+  resultEl.style.justifyContent = "space-evenly";
+  resultEl.style.alignItems = "center";
+  const scoreEl = document.getElementById("score");
+  const percentScore = (score * 100) / (quizData.length * 5);
+  scoreEl.innerHTML = `${percentScore} %`;
+
+  const message = messageShow(percentScore);
+  document.getElementById("message").innerHTML = message;
+}
+
+// function to show message
+function messageShow(per) {
+  let msg = "";
+
+  if (per >= 50) {
+    msg = "Congratulations!";
+  } else {
+    msg = "Try Again!";
+  }
+
+  return msg;
+}
+
+// call loadQuiz function
+let currentQuiz = loadQuiz();
+
+// Eventlistener on submit
+submitBtn.addEventListener("click", isAnswerSelected);
